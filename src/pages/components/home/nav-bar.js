6c333ef7a49bb7d9fc329c6d2588cf86c0b22a58
Elizabeth Pearce <eliz.mpearce@gmail.com>
2023-08-09 17:32:06 -0500

    added mobile responsiveness

--- src/pages/components/home/nav-bar.js
+++ src/pages/components/home/nav-bar.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 
   const NavBar = ({ isLoggedIn, handleLogout }) => {
-    const navigate = useNavigate();
+    const navigate = useNavigate(); 
+
   
     const handleLogoutClick = () => {
       localStorage.removeItem('authToken');
       handleLogout();
       navigate('/');
     };
+
   return (
     <div className="nav-bar-button-wrapper">
       <NavLink exact to="/" className="btn-special-nav-bar">Home</NavLink>
